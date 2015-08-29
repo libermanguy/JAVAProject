5766745ffd6e7d2e76fad34afc283eda3213c802
omripolnikviat <omripolnikviat@10.0.0.1>
2015-08-29 13:16:20 +0300

    Updated JavaDoc for the new Classes

--- JavaProject/src/controller/Command.java
+++ JavaProject/src/controller/Command.java
@@ -1,7 +1,19 @@
 package controller;
 
+/**
+* <h1>The interface Command</h1>
+* The command interface as part of Commands design pattern
+*
+* @author  Omri Polnikviat
+* @version 1.0
+* @since   2015-08-29 
+*/
+
 public interface Command {
 	
+	/**
+	 * Do command.
+	 */
 	public void doCommand();
 
 }
--- JavaProject/src/view/CLI.java
+++ JavaProject/src/view/CLI.java
@@ -1,3 +1,12 @@
+/**
+* <h1>The Class CLI</h1>
+* The command line class 
+*
+* @author  Omri Polnikviat
+* @version 1.0
+* @since   2015-08-29 
+*/
+
 package view;
 
 import java.io.BufferedReader;
@@ -7,19 +16,37 @@ import java.util.HashMap;
 
 import controller.Command;
 
+/**
+ * The Class CLI.
+ */
 public class CLI extends Thread{
 	
+	/** The input . */
 	protected BufferedReader in;
+	
+	/** The output. */
 	protected PrintWriter out;
+	
+	/** String to Commands HashMap. */
 	HashMap<String,Command> cmdsHM;
 
 	
+	/**
+	 * Instantiates a new cli.
+	 *
+	 * @param input the input
+	 * @param output the output
+	 * @param commandsHM the commands hm
+	 */
 	public CLI(BufferedReader input,PrintWriter output,HashMap<String,Command> commandsHM){
 		in=input;
 		out=output;
 		cmdsHM=commandsHM;
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Thread#start()
+	 */
 	public void start(){
 		new Thread(new Runnable() {
 			@Override
