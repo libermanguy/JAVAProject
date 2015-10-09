9092b8b5986b312f218b7a1563d2f37d3aa73da6
omripolnikviat <omripolnikviat@10.0.0.7>
2015-10-09 18:40:51 +0300

    Merge

--- JavaProject/src/demo/TestMVC.java
+++ JavaProject/src/demo/TestMVC.java
@@ -14,3 +14,12 @@ public class TestMVC {
 	}
 
 }
+
+/*
+***********test commands******************
+
+generate 3d maze testmaze 10 10 10
+save maze testmaze c:\temp\tm.maz
+load maze c:\temp\tm.maz testmaze2
+
+*/
