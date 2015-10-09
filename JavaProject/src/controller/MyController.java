288fab24521dd26852cdfac81fd64126f0bb569a
libermanguy <guyliberman@gmail.com>
2015-10-09 15:00:32 +0300

    updating

--- JavaProject/src/controller/MyController.java
+++ JavaProject/src/controller/MyController.java
@@ -6,6 +6,8 @@ import view.MyView;
 public class MyController implements Controller{
 Model model;
 MyView view;
+
+
 @Override
 public void PleaseTellView(String update) {
 	System.out.println(update);
