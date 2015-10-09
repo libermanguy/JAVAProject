08b56a7f861bbd3b75bd60ec153f2521a8111be4
libermanguy <guyliberman@gmail.com>
2015-10-09 16:02:05 +0300

    updated commands

--- JavaProject/src/controller/MyController.java
+++ JavaProject/src/controller/MyController.java
@@ -1,11 +1,32 @@
 package controller;
 
+import java.util.HashMap;
+
+import algorithms.search.Solution;
+import general.Position;
 import model.Model;
+import model.SearchableMaze;
 import view.MyView;
 
 public class MyController implements Controller{
 Model model;
 MyView view;
+HashMap<String,Command> commands;
+
+public MyController() {
+	super();
+	commands.put("dir", new dirCommand());
+	commands.put("generate 3d maze", new generateCommand());
+	commands.put("display", new displayMazeCommand());
+	commands.put("display cross section", new displayCrossCommand());
+	commands.put("save maze", new saveCommand());
+	commands.put("load maze", new loadCommand());
+	commands.put("maze size", new mazeSizeCommand());
+	commands.put("file size", new fileSizeCommand());
+	commands.put("solve", new solveCommand());
+	commands.put("display solution", new dislpaySolutionCommand());
+	commands.put("exit", new exitCommand());
+}
 
 
 @Override
@@ -21,4 +42,127 @@ public void PleaseTellModel(String update) {
 public void AskViewToPresent(Object[] arg) {
 	view.display(arg);
 }
+
+public class dirCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class generateCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class displayMazeCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class displayCrossCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class saveCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class loadCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class mazeSizeCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class fileSizeCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class solveCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class dislpaySolutionCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+public class exitCommand implements Command
+{
+
+	@Override
+	public void doCommand(String args[]) {
+		// TODO Auto-generated method stub
+		
+	}
+		
+}
+
+
 }
